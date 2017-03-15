ec56d71137bf237ba98a987c23137882813fdca0
d4rkb1ue <7874364@gmail.com>
2017-03-15 18:26:15 +0800

    reactize hello world

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,21 @@ var path = require("path");
 
 module.exports = {
 	entry: {
-		app: ["./main.js"]
+		app: ["./main.jsx"]
 	},
 	output: {
 		path: path.resolve(__dirname, "build"),
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+		{
+			test: /\.js[x]?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}]
 	}
 };
